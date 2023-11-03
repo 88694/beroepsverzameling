5b6f328bb44e715ed1c2789c3a39fff5202afefd
88694 <88694@glr.nl>
2023-11-03 13:38:31 +0100

    updates and working store page

--- store.php
+++ store.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./assets/CSS/style.css">
     <link rel="icon" href="./assets/Images/logo.png">
+    <link rel="stylesheet" href="./assets/CSS/collect.css">
     <script src="./assets/JavaScript/Script.js"></script>
     <title> Verzameling </title>
 </head>
@@ -21,14 +22,38 @@
             <div class="menu">
                 <ul>
                     <li><a href="index.html">Home</a></li>
-                    <li><a href="store.html">Store</a></li>
+                    <li><a href="store.php">Store</a></li>
                     <li><a href="reviews.html">Reviews</a></li>
                 </ul>
             </div>
         </div>
-        <div id="dumbell">
-            <img draggable="false" src="/assets/Images/logo.png"> 
-        </div>
+        <div class="collection-box">         
+    <?php
+    include('./assets/src/db.php');
+    $stmt = $db->prepare("SELECT * FROM collectie");
+    $stmt->execute();
+
+    echo '<div class="collection-box">';
+
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        echo '<div class="collection-item">';
+        echo '<div class="collection-left">';
+        echo '<h3 id="naam_product">' . $row['naam'] . '</h3>';
+        echo '<p id="kleur">' . $row['kleur'] . '</p>';
+        echo '<h3 id="uitgaveJaar">' . $row['jaar'] . '</h3>';
+        echo '<p id="informatie">' . $row['info'] . '</p>';
+        echo '</div>';
+        echo '<div class="collection-right">';
+        echo '<img src="' . $row['image'] . '" alt="">';
+        echo '</div>';
+        echo '</div>';
+    }
+
+    echo '</div>';
+    ?>
+</div>
+
+
 </body>
 
 </html>
